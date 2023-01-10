5bd328f2e3585ab0df4440ec2b4179cc0eb9d826
Gustavo Batista <gustavobatistafig@hotmail.com>
2023-01-10 18:53:04 -0300

    criando testes unitários e modelos

---
+++ src/main/java/escola/Indicacao.java
@@ -0,0 +1,30 @@
+package escola;
+
+public class Indicacao {
+
+	private Aluno indicado;
+	private Aluno indicante;
+
+	public Indicacao(Aluno indicado, Aluno indicante) {
+		super();
+		this.indicado = indicado;
+		this.indicante = indicante;
+	}
+
+	public Aluno getIndicado() {
+		return indicado;
+	}
+
+	public void setIndicado(Aluno indicado) {
+		this.indicado = indicado;
+	}
+
+	public Aluno getIndicante() {
+		return indicante;
+	}
+
+	public void setIndicante(Aluno indicante) {
+		this.indicante = indicante;
+	}
+
+}
